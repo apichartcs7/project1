12d55f144287cfb8fba32876a3e164c5c41d0fca
apichartcs7 <41360339+apichartcs7@users.noreply.github.com>
2018-07-18 15:54:34 +0700

    try other script

--- myproject_build.groovy
+++ myproject_build.groovy
@@ -1,25 +1,37 @@
 // MyProject-Build.groovy
 
-def gitUrl = "https://github.com/apichartcs7/project1.git"
+// def gitUrl = "https://github.com/apichartcs7/project1.git"
 
-job("MyProject-Build") {
-    description "Builds MyProject from master branch."
-    parameters {
-        stringParam('COMMIT', 'HEAD', 'Commit to build')
-    }
+// job("MyProject-Build") {
+//     description "Builds MyProject from master branch."
+//     parameters {
+//         stringParam('COMMIT', 'HEAD', 'Commit to build')
+//     }
+//     scm {
+//         git {
+//             remote {
+//                 url gitUrl.
+//                 //branch "origin/master"
+//             }
+//             extensions {
+//                 wipeOutWorkspace()
+//                 localBranch master
+//             }
+//         }
+//     }
+//     steps {
+//         shell "Look: I'm building master!"
+//     }
+// }
+
+job('DSL-Tutorial-1-Test') {
     scm {
-        git {
-            remote {
-                url gitUrl
-                //branch "origin/master"
-            }
-            extensions {
-                wipeOutWorkspace()
-                localBranch master
-            }
-        }
+        git('git://github.com/apichartcs7/project1.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
     }
     steps {
-        shell "Look: I'm building master!"
+        maven('-e clean test')
     }
 }
